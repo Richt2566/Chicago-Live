7a2d39c4813f66899b9e58a0710547133eeee037
stabrr <bobbystabile@yahoo.com>
2017-10-24 19:40:45 -0500

    added ids for dates, changed date format, updated query and grabbed more data

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -6,6 +6,7 @@ var locationsEmpty = [];
 $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 2, // Creates a dropdown of 15 years to control year,
+    format: 'yyyy-mm-dd',
     today: 'Today',
     clear: 'Clear',
     close: 'Ok',
@@ -13,44 +14,55 @@ $('.datepicker').pickadate({
 });
 
 //-----------------------------------------------------------
+$("#submit-btn").on("click", function(event){
+  event.preventDefault();
 
 // constructing a queryURL variable we will use instead of the literal string inside of the ajax method
-var title = "Ticket Master";
-var genre = "Rock";
-var startDate = "2017-10-23T00:00:00Z";
-var endDate ="2017-10-26T00:00:00Z";
-var size="50";
-var apiKey="qq8XdJrLt8geS8g2CUjbY9sqKk8crlQw";
-var queryURL = "https:app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=Chicago&classificationName=music&classificationName="+genre+"&startDateTime="+startDate+"&endDateTime="+endDate+"&size="+size+"&apikey="+apiKey;
+  var startDates = $("#startDate").val();
+  var endDates = $("#endDate").val();
+  var time = "T00:00:00Z";
+  console.log(startDates);
+  var title = "Ticket Master";
+  var genre = "Rock";
+  var startDate = startDates.concat(time);
+  var endDate = endDates.concat(time);
+  var size="50";
+  var apiKey="qq8XdJrLt8geS8g2CUjbY9sqKk8crlQw";
+  var queryURL = "https:app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=Chicago&classificationName=music&classificationName="+genre+"&startDateTime="+startDate+"&endDateTime="+endDate+"&size="+size+"&apikey="+apiKey;
 
-//"https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=Chicago&endDateTime=2017-10-28T00:00:00Z&startDateTime=2017-10-23T00:00:00Z&classificationId=KZFzniwnSyZfZ7v7nJ&classificationName=pop&size=31&apikey="+ apiKey;
+  //"https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=Chicago&endDateTime=2017-10-28T00:00:00Z&startDateTime=2017-10-23T00:00:00Z&classificationId=KZFzniwnSyZfZ7v7nJ&classificationName=pop&size=31&apikey="+ apiKey;
 
-//"https://app.ticketmaster.com/classification/v2/Id=KZFzniwnSyZfZ7v7nJ&apikey"+apiKey;
-// "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=Chicago&keyword=katy perry&apikey="+ apiKey;
-//"https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=chicago&apikey="+apiKey;
+  //"https://app.ticketmaster.com/classification/v2/Id=KZFzniwnSyZfZ7v7nJ&apikey"+apiKey;
+  // "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=Chicago&keyword=katy perry&apikey="+ apiKey;
+  //"https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=chicago&apikey="+apiKey;
 
-//the ajax call
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).done(function(response) {
-    console.log(response);
-    // console.log(response.Runtime);
-        var events = response._embedded.events;
-  console.log(events);
-  var myObject = {
-    "shows": []
-  };
-  for(var i=0;i<events.length;i++){
-    var aShow ={
-      "name": events[i].name,
-      "date": events[i].dates.start.localDate
-     } ;
-    console.log(aShow);
-    myObject.shows.push(aShow);
-  }
+  //the ajax call
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      console.log(response);
+      // console.log(response.Runtime);
+          var events = response._embedded.events;
+    console.log(events);
+    var myObject = {
+      "shows": []
+    };
+    for(var i=0;i<events.length;i++){
+      var aShow ={
+        "name": events[i].name,
+        "date": events[i].dates.start.localDate,
+        "venue": events[i]._embedded.venues[0].name,
+        "latitude": events[i]._embedded.venues[0].location.latitude,
+        "longitude": events[i]._embedded.venues[0].location.longitude
+       } ;
+      console.log(aShow);
+      myObject.shows.push(aShow);
+    }
 
-});
+  });
+
+});  
 
 //-----------------------------------------------------------
 
@@ -79,10 +91,10 @@ function changeSrc() {
 
 $(document).ready(function() {
     // Emily what is this?
-    $('select').material_select();
+   $('select').material_select();
 
     //
-    $("#submit-button").on("click", function(){
+   $("#submit-button").on("click", function(){
 
       //once submit gets clicked ten bands show up 
       //from the div area - maybe reworking into for loop
@@ -96,7 +108,9 @@ $(document).ready(function() {
       //   column.append("<tr>" + something + "</tr>");
       //$("#button-pit").append(column);
 
-      loadIframe()
-      changeSrc();
+     loadIframe()
+     changeSrc();
     });
 });
+
+//$(document).on("click", "#submit-btn", onSubmit);
