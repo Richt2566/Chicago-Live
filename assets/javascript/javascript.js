bed6744256d8f3141bd4d8d662e6f7da4cf79e76
Muriel <muriel.mangubat@gmail.com>
2017-10-30 19:25:32 -0500

    add js code for fav area

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,9 +1,12 @@
 var config = {
-    apiKey: "AIzaSyC_iCNRw1-_EefEtmgoyiHN5XdB3UGisvA",
-    authDomain: "brew-database-97a1f.firebaseapp.com",
-    databaseURL: "https://brew-database-97a1f.firebaseio.com",
-    storageBucket: "brew-database-97a1f.appspot.com",
+    apiKey: "AIzaSyCKhCYuatzoxWsG9RohKQRGLn05cpE5S_c",
+    authDomain: "concertfinderchicago.firebaseapp.com",
+    databaseURL: "https://concertfinderchicago.firebaseio.com",
+    projectId: "concertfinderchicago",
+    storageBucket: "concertfinderchicago.appspot.com",
+    messagingSenderId: "792833060372"
 };
+
 
 // make sure they are connecting
 firebase.initializeApp(config);
@@ -26,123 +29,136 @@ $('.datepicker').pickadate({
 //var genreChange = false;
 
 //-----------------------------------------------------------
-$("#submit-btn").on("click", function(event){
+$("#submit-btn").on("click", function(event) {
 
-  // this prevents the page from reloading
-  event.preventDefault();
+    // this prevents the page from reloading
+    event.preventDefault();
 
-  // constructing a queryURL variable we will use instead of the literal string inside of the ajax method
-  var startDates = $("#startDate").val();
-  var endDates = $("#endDate").val();
-  var time = "T00:00:00Z";
-  var title = "Ticket Master";
-  var genre = $("#genre").val();
-  var startDate = startDates.concat(time);
-  var endDate = endDates.concat(time);
-  var size = 20;
-  var apiKey="qq8XdJrLt8geS8g2CUjbY9sqKk8crlQw";
-  var queryURL = "https:app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=Chicago&classificationName=music&classificationName="+genre+"&startDateTime="+startDate+"&endDateTime="+endDate+"&size="+size+"&apikey="+apiKey;
+    // constructing a queryURL variable we will use instead of the literal string inside of the ajax method
+    var startDates = $("#startDate").val();
+    var endDates = $("#endDate").val();
+    var time = "T00:00:00Z";
+    var title = "Ticket Master";
+    var genre = $("#genre").val();
+    var startDate = startDates.concat(time);
+    var endDate = endDates.concat(time);
+    var size = 20;
+    var apiKey = "qq8XdJrLt8geS8g2CUjbY9sqKk8crlQw";
+    var queryURL = "https:app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=Chicago&classificationName=music&classificationName=" + genre + "&startDateTime=" + startDate + "&endDateTime=" + endDate + "&size=" + size + "&apikey=" + apiKey;
 
-  var myShows = {
-    "shows": []
-  };
+    var myShows = {
+        "shows": []
+    };
 
-  //"https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=Chicago&endDateTime=2017-10-28T00:00:00Z&startDateTime=2017-10-23T00:00:00Z&classificationId=KZFzniwnSyZfZ7v7nJ&classificationName=pop&size=31&apikey="+ apiKey;
-  //"https://app.ticketmaster.com/classification/v2/Id=KZFzniwnSyZfZ7v7nJ&apikey"+apiKey;
-  // "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=Chicago&keyword=katy perry&apikey="+ apiKey;
-  //"https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=chicago&apikey="+apiKey;
+    //"https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=Chicago&endDateTime=2017-10-28T00:00:00Z&startDateTime=2017-10-23T00:00:00Z&classificationId=KZFzniwnSyZfZ7v7nJ&classificationName=pop&size=31&apikey="+ apiKey;
+    //"https://app.ticketmaster.com/classification/v2/Id=KZFzniwnSyZfZ7v7nJ&apikey"+apiKey;
+    // "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=Chicago&keyword=katy perry&apikey="+ apiKey;
+    //"https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=chicago&apikey="+apiKey;
 
     //the ajax call
     $.ajax({
         url: queryURL,
         method: "GET"
-      }).done(function(response) {
-          var events = response._embedded.events;
+    }).done(function(response) {
+        var events = response._embedded.events;
         myShows = {
-          "shows": []
-      };
+            "shows": []
+        };
 
-      for(var i=0;i<events.length;i++){
-        var aShow ={
-          "name": events[i].name,
-          "date": events[i].dates.start.localDate,
-          "venue": events[i]._embedded.venues[0].name,
-          "photoURL": events[i].images[0].url,
-          "ticketURL": events[i].url
-          //"latitude": events[i]._embedded.venues[0].location.latitude,
-          //"longitude": events[i]._embedded.venues[0].location.longitude
-        } ;
+        for (var i = 0; i < events.length; i++) {
+            var aShow = {
+                "name": events[i].name,
+                "date": events[i].dates.start.localDate,
+                "venue": events[i]._embedded.venues[0].name,
+                "photoURL": events[i].images[0].url,
+                "ticketURL": events[i].url
+                //"latitude": events[i]._embedded.venues[0].location.latitude,
+                //"longitude": events[i]._embedded.venues[0].location.longitude
+            };
 
-        myShows.shows.push(aShow);
-        
-        var myButton = $("<button class='api-btn'>" + events[i].name + "<br>"
-         + events[i].dates.start.localDate + "</button>");
-        
-        //adding attribute to show as a string
-        myButton.attr("data-show", i);
+            myShows.shows.push(aShow);
 
-        //adding all info for buttons to the div we set
-        $('.concert-btn').append(myButton);
+            var myButton = $("<button class='api-btn'>" + events[i].name + "<br>" +
+                events[i].dates.start.localDate + "</button>");
 
-        // create a click function for the results of the api
-        myButton.click(function() {
+            //adding attribute to show as a string
+            myButton.attr("data-show", i);
 
-          // grabs the index of show
-          var showIndex = $(this).attr('data-show');
+            //adding all info for buttons to the div we set
+            $('.concert-btn').append(myButton);
 
-          // calling function that changes 'src'
-          changeSrc(myShows.shows[showIndex].venue);
+            // create a click function for the results of the api
+            myButton.click(function() {
 
-          // calling function that pupulates card
-          makeCard(myShows.shows[showIndex].name, myShows.shows[showIndex].photoURL, myShows.shows[showIndex].venue, myShows.shows[showIndex].date);
+                // grabs the index of show
+                var showIndex = $(this).attr('data-show');
 
-            $(".card").show();
+                // calling function that changes 'src'
+                changeSrc(myShows.shows[showIndex].venue);
 
-            $(".btn-floating").on("click", function(){
-              var thisShow = myShows.shows[showIndex]
-              
-              database.ref().push(thisShow);
+                // calling function that pupulates card
+                makeCard(myShows.shows[showIndex].name, myShows.shows[showIndex].photoURL, myShows.shows[showIndex].venue, myShows.shows[showIndex].date);
 
-              $(".btn-floating").html('<i class="material-icons">star</i></a>');
-            })
-        });
-      }
+                $(".card").show();
+
+                $(".btn-floating").on("click", function() {
+                    var thisShow = myShows.shows[showIndex]
+
+                    database.ref().push(thisShow);
+
+                    $(".btn-floating").html('<i class="material-icons">star</i></a>');
+                })
+            });
+        }
     });
-});  
+});
 
 
 //-----------------------------------------------------------
 
 // this will store the info to firebase
-database.ref().on("child_added", function(snapshot){
+database.ref().on("child_added", function(snapshot) {
 
-  $("#fave-area").append(snapshot.val().name);
-  $("#fave-area").append(snapshot.val().venue);
-  $("#fave-area").append(snapshot.val().date);
-  
+    // $("#fave-area").append(snapshot.val().name);
+    // $("#fave-area").append(snapshot.val().venue);
+    // $("#fave-area").append(snapshot.val().date);
+// 
+    var html = [
+        '<div class="row center-align">',
+        '<div class="col s6 m4 l4">',
+        '<div class="card">',
+        '<div class="card-image">',
+        '<img id="card-img" src="">',
+        '<span class="card-title">' + snapshot.val().name + snapshot.val().venue + snapshot.val().date+'</span>',
+        '<a class="btn-floating halfway-fab waves-effect waves-light">',
+        ' <i class="material-icons">star_outline</i> </a></div>'
+    ].join("")
+    $('#fave-area').append(html)
+
 });
+
 
 //-----------------------------------------------------------
 
 // this function populates the info on the card
 function makeCard(myCard, myCard2, myCard3, myCard4) {
-  
-  var myText = myCard;
 
-  $("#card-p").text(myText);
-  $("#card-img").attr('src', myCard2);
-  $("#card-v").text(myCard3);
-  $("#card-t").text(myCard4);
+    var myText = myCard;
+
+    $("#card-p").text(myText);
+    $("#card-img").attr('src', myCard2);
+    $("#card-v").text(myCard3);
+    $("#card-t").text(myCard4);
 }
 
 //this will be the function that changes the src in the map
 function changeSrc(myobj) {
 
-  // we grab what the api specifies
-  var userLocation = myobj;
+    // we grab what the api specifies
+    var userLocation = myobj;
 
-  //once submit gets clicked we change the path of the iframe to what the user has typed
-  $("#myFrame").attr('src', "https://www.google.com/maps/embed/v1/search?q=" + userLocation + "&key=AIzaSyB7ydrZE1U4_y3TjyeaO2aVyfWzxUnxKuk");
+    //once submit gets clicked we change the path of the iframe to what the user has typed
+    $("#myFrame").attr('src', "https://www.google.com/maps/embed/v1/search?q=" + userLocation + "&key=AIzaSyB7ydrZE1U4_y3TjyeaO2aVyfWzxUnxKuk");
 }
 
 //-----------------------------------------------------------
@@ -150,7 +166,7 @@ function changeSrc(myobj) {
 // when the document loads this happens...
 $(document).ready(function() {
 
-  // materialize jquery for selection boxes
-  $('select').material_select();
+    // materialize jquery for selection boxes
+    $('select').material_select();
 
 });
